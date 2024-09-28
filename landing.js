c968fa3b4c4bdc84c6cf578bb0ba13feda5d0d18
Nikihashi <160816098+Nikihashi@users.noreply.github.com>
2024-09-28 11:02:08 +0530

    Create landing.js

---
+++ landing.js
@@ -0,0 +1,16 @@
+// let tl = gsap.timeline();
+
+// tl.from(".x h1",{
+//     y:30,
+//     opacity:0,
+//     duration:.5,
+//     stagger: .5,
+//     backgroundColor:"transparent",
+//     scrollTrigger:{
+//         trigger: ".page1",
+//         scroller: "body",
+//         start: "bottom 50%",
+//         scrub:2,
+        
+//     }
+// })
